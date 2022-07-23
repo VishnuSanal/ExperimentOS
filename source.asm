0f892870fe951d5714ff8a454bb5b2a35d15ca8f
VishnuSanal <t.v.s10123@gmail.com>
2022-07-23 11:38:53 +0530

    alphabets

--- source.asm
+++ source.asm
@@ -1,16 +1,16 @@
 mov ah, 0x0e
 mov al, 'A'
-int 0x10
 
 label:
-	inc al
 	int 0x10
+	inc al
 
-	cmp al, 'Z'
+	cmp al, 'Z' + 1
 	je exit
 
 	jmp label
 exit:
+	jmp $
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
